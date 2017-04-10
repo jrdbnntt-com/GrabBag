85e7b15b48d1fd0d3be81099681765466096d3e0
Alexis Taylor <alexismtaylor2@gmail.com>
2017-04-10 00:23:48 -0400

    Created UIs

---
+++ app/src/main/java/com/jrdbnntt/cop4656/grabbag/JoinGroupActivity.java
@@ -0,0 +1,21 @@
+package com.jrdbnntt.cop4656.grabbag;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class JoinGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_group);
+    }
+
+    public void clickJoinGroup(View view)
+    {
+        Intent intent = new Intent(this, GroupScreenActivity.class);
+        startActivity(intent);
+    }
+}
