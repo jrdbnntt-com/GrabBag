5abdd5a9ff6a579b77c7d0a03329e82ccee42dec
Alexis Taylor <alexismtaylor2@gmail.com>
2017-04-13 23:30:15 -0400

    Edited UI

--- app/src/main/java/com/jrdbnntt/cop4656/grabbag/app/CreateAccountActivity.java
+++ app/src/main/java/com/jrdbnntt/cop4656/grabbag/app/CreateAccountActivity.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jrdbnntt.cop4656.grabbag.R;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    EditText etUsername, etPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
     }
 
     public void submitAcct(View view)
     {
+        String username, password;
+        username = etUsername.toString();
+        password = etPassword.toString();
+        /*
+            Check to see if username is not in the DB
+            If true, pass intent to next activity, if false alert user with toast
+                and have them re-enter their username and password
+
+           Toast.makeText(this, "Invalid username or password.", Toast.LENGTH_LONG).show();
+           etUsername.setText("");  //resetting username
+           etPassword.setText("");  //resetting password
+        */
         Intent intent = new Intent(this, GroupChoiceActivity.class);
         startActivity(intent);
     }
