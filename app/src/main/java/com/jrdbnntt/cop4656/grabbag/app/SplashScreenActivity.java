a165dd85a7a37021c049f666195e479a8f72bfc6
cristianpalencia <cristianpalencia18@gmail.com>
2017-04-16 16:48:52 -0400

    Shorten splash screen

--- app/src/main/java/com/jrdbnntt/cop4656/grabbag/app/SplashScreenActivity.java
+++ app/src/main/java/com/jrdbnntt/cop4656/grabbag/app/SplashScreenActivity.java
@@ -9,7 +9,7 @@ import android.view.Window;
 import com.jrdbnntt.cop4656.grabbag.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
